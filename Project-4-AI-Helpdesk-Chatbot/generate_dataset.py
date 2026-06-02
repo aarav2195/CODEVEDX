9a3f8dadcc26968482f48accf95a50131a6ecc88
Aarav Shah <arav2195@gmail.com>
2026-06-02 15:29:11 +0530

    Added Project 4 initial structure

---
+++ Project-4-AI-Helpdesk-Chatbot/generate_dataset.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+faq_data={
+    "question":[
+        "how to reset password",
+        "how to apply leave",
+        "what are office timings",
+        "how to contact hr",
+        "where is employee handbook",
+        "how to update profile",
+        "how to request laptop support",
+        "how to check leave balance",
+        "how to access payroll",
+        "how to contact it support"
+    ],
+
+    "answer":[
+        "Use the reset password portal.",
+        "Submit a leave request trough the HR portal.",
+        "Office timings are 9 AM to 6 PM.",
+        "Email hr@company.com.",
+        "Available on the company intranet.",
+        
+
+    ]
+}
